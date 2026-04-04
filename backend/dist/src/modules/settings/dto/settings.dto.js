8bad515ec9f4f8169885bcfb8ed560e0acbbe595
office-acc <office.acc.2023.pral@gmail.com>
2026-04-05 00:38:33 +0500

    build fixes

--- backend/dist/src/modules/settings/dto/settings.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateSettingsSchema = void 0;
-const zod_1 = require("zod");
-exports.updateSettingsSchema = zod_1.z.object({
-    overspendingMode: zod_1.z.enum(['block', 'warn'])
-});
